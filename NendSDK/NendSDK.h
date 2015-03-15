440b92a8c4e6469ae219f44914b70ae265e9ece7
Jun Hashimoto <fusui.dreamer@gmail.com>
2015-03-15 12:07:48 +0900

    add NendSDK project

---
+++ NendSDK/NendSDK.h
@@ -0,0 +1,19 @@
+//
+//  NendSDK.h
+//  NendSDK
+//
+//  Created by jun.hashimoto on 2015/03/15.
+//  Copyright (c) 2015年 jun.hashimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NendSDK.
+FOUNDATION_EXPORT double NendSDKVersionNumber;
+
+//! Project version string for NendSDK.
+FOUNDATION_EXPORT const unsigned char NendSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NendSDK/PublicHeader.h>
+
+
